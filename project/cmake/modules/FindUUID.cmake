1c7e4633dfe697c68964608d34b040d07995aa5d
Christian Fetzer <fetzer.ch@googlemail.com>
2016-01-16 16:55:13 +0100

    [cmake] Modernize FindUUID.cmake and the crossguid CMakeLists.txt

--- project/cmake/modules/FindUUID.cmake
+++ project/cmake/modules/FindUUID.cmake
@@ -1,19 +1,43 @@
-# - Try to find UUID
-# Once done this will define
+#.rst:
+# FindUUID
+# --------
+# Finds the libuuid library
+#
+# This will will define the following variables::
 #
 # UUID_FOUND - system has libuuid
 # UUID_INCLUDE_DIRS - the libuuid include directory
-# UUID_LIBRARIES - The libuuid libraries
+# UUID_LIBRARIES - the libuuid libraries
+#
+# and the following imported targets::
+#
+#   UUID::UUID   - The libuuid library
 
-find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules (UUID uuid)
-else()
-  find_path(UUID_INCLUDE_DIRS uuid/uuid.h)
-  find_library(UUID_LIBRARIES uuid)
+  pkg_check_modules(PC_UUID uuid QUIET)
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(UUID DEFAULT_MSG UUID_INCLUDE_DIRS UUID_LIBRARIES)
+find_path(UUID_INCLUDE_DIR uuid/uuid.h
+                           PATHS ${PC_UUID_INCLUDEDIR})
+find_library(UUID_LIBRARY uuid
+                          PATHS ${PC_UUID_LIBRARY})
+set(UUID_VERSION ${PC_UUID_VERSION})
 
-mark_as_advanced(UUID_INCLUDE_DIRS UUID_LIBRARIES)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(UUID
+                                  REQUIRED_VARS UUID_LIBRARY UUID_INCLUDE_DIR
+                                  VERSION_VAR UUID_VERSION)
+
+if(UUID_FOUND)
+  set(UUID_LIBRARIES ${UUID_LIBRARY})
+  set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
+
+  if(NOT TARGET UUID::UUID)
+    add_library(UUID::UUID UNKNOWN IMPORTED)
+    set_target_properties(UUID::UUID PROPERTIES
+                                     IMPORTED_LOCATION "${UUID_LIBRARY}"
+                                     INTERFACE_INCLUDE_DIRECTORIES "${UUID_INCLUDE_DIR}")
+  endif()
+endif()
+
+mark_as_advanced(UUID_INCLUDE_DIR UUID_LIBRARY)
