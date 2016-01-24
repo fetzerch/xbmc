9aac7b2b1196bff0f4e571b403ea6eaf2ab542de
Christian Fetzer <fetzer.ch@googlemail.com>
2016-01-24 22:05:34 +0100

    [cmake] Wrapper libraries for OSX
    Credits to Memphiz.
    
    Adds darwin implementation for core_link_library (incl. copying the wrapper.def).

--- project/cmake/scripts/darwin/macros.cmake
+++ project/cmake/scripts/darwin/macros.cmake
@@ -1,41 +1,50 @@
 function(core_link_library lib wraplib)
-#  set(export -undefined dynamic_lookup
-#             `cat ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cores/dll-loader/exports/wrapper.def`
-#             ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cores/dll-loader/exports/CMakeFiles/wrapper.dir/wrapper.c.o)
-#  set(check_arg "")
-#  if(TARGET ${lib})
-#    set(target ${lib})
-#    set(link_lib ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/${lib}/${lib}.a)
-#    set(check_arg ${ARGV2})
-#    set(data_arg  ${ARGV3})
-#  else()
-#    set(target ${ARGV2})
-#    set(link_lib ${lib})
-#    set(check_arg ${ARGV3})
-#    set(data_arg ${ARGV4})
-#  endif()
-#  if(check_arg STREQUAL "export")
-#    set(export ${export}
-#        -Wl,--version-script=${ARGV3})
-#  elseif(check_arg STREQUAL "nowrap")
-#    set(export ${data_arg})
-#  elseif(check_arg STREQUAL "extras")
-#    foreach(arg ${data_arg})
-#      list(APPEND export ${arg})
-#    endforeach()
-#  endif()
-#  get_filename_component(dir ${wraplib} PATH)
-#  add_custom_command(OUTPUT ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
-#                     COMMAND ${CMAKE_COMMAND} -E make_directory ${dir}
-#                     COMMAND ${CMAKE_C_COMPILER}
-#                     ARGS    -Wl,-force_load
-#                             ${link_lib}
-#                             -lm
-#                             -shared -o ${CMAKE_BINARY_DIR}/${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
-#                             ${export}
-#                     DEPENDS ${target} wrapper.def wrapper)
-#  list(APPEND WRAP_FILES ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX})
-#  set(WRAP_FILES ${WRAP_FILES} PARENT_SCOPE)
+  set(export -bundle -undefined dynamic_lookup -read_only_relocs suppress
+             -Wl,-alias_list,${CORE_BUILD_DIR}/cores/dll-loader/exports/wrapper.def
+             ${CORE_BUILD_DIR}/cores/dll-loader/exports/CMakeFiles/wrapper.dir/wrapper.c.o)
+  set(check_arg "")
+  if(TARGET ${lib})
+    set(target ${lib})
+    set(link_lib ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/${lib}/${lib}.a)
+    set(check_arg ${ARGV2})
+    set(data_arg  ${ARGV3})
+  else()
+    set(target ${ARGV2})
+    set(link_lib ${lib})
+    set(check_arg ${ARGV3})
+    set(data_arg ${ARGV4})
+  endif()
+  if(check_arg STREQUAL "export")
+    set(export ${export}
+        -Wl,--version-script=${ARGV3})
+  elseif(check_arg STREQUAL "nowrap")
+    set(export ${data_arg})
+  elseif(check_arg STREQUAL "extras")
+    foreach(arg ${data_arg})
+      list(APPEND export ${arg})
+    endforeach()
+  endif()
+  get_filename_component(dir ${wraplib} PATH)
+
+  # We can't simply pass the linker flags to the args section of the custom command
+  # because cmake will add quotes around it (and the linker will fail due to those).
+  # We need to do this handstand first ...
+  separate_arguments(CUSTOM_COMMAND_ARGS_LDFLAGS UNIX_COMMAND "${CMAKE_SHARED_LINKER_FLAGS}")
+
+  add_custom_command(OUTPUT ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
+                     COMMAND ${CMAKE_COMMAND} -E make_directory ${dir}
+                     COMMAND ${CMAKE_C_COMPILER}
+                     ARGS    ${CUSTOM_COMMAND_ARGS_LDFLAGS} ${export} -Wl,-force_load ${link_lib}
+                             -o ${CMAKE_BINARY_DIR}/${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
+                     DEPENDS ${target} wrapper.def wrapper
+                     VERBATIM)
+
+  # Uncomment to create wrap_<lib> targets for debugging
+  #get_filename_component(libname ${wraplib} NAME_WE)
+  #add_custom_target(wrap_${libname} ALL DEPENDS ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX})
+
+  list(APPEND WRAP_FILES ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX})
+  set(WRAP_FILES ${WRAP_FILES} PARENT_SCOPE)
 endfunction()
 
 function(find_soname lib)
